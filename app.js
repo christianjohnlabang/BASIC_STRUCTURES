ece804c687a48d6eb9b3e5f705d00b5e64785c5e
christian <labang74@gmail.com>
2016-07-07 18:55:29 +0800

    Basic_Structures

---
+++ app.js
@@ -0,0 +1,40 @@
+var age=15;
+var pi=3.1416;
+var date= new Date();
+var computeArea= function(l,w){
+return l*w;
+};
+var name="Christianjohnlabang";
+console.log('Age'+age);
+console.log('PI'+ pi.toFixed(2));
+console.log(date);
+console.log(name);
+console.log(computeArea(10,60));
+
+var x= 1;
+var y=null;
+var waladiha= undefined;
+ 
+ if(x)
+ console.log('x is true');
+ 
+ if(!y)
+ console.log('y is false');
+ 
+ if (!waladiha)
+ console.log('wala diha is undefined');
+ 
+ 
+ //================JESHOOON IS LOVE;
+ 
+ var jsonObject= {
+     name: 'Jason',
+     lastname: 'Calope',
+     address: 'Catigbian'
+ };
+ 
+ console.log(jsonObject.name);
+ console.log(jsonObject.lastname);
+ console.log(jsonObject.address);
+ console.log(jsonObject);
+  
